f3b5d3f57d7e08525b911268af977d3b9becd686
kinzlee <chinemeremnwoga@gmail.com>
2020-08-06 21:17:34 +0100

    rounding up navigation

---
+++ constants/colors.js
@@ -0,0 +1,3 @@
+export default {
+  primaryColor: "#183076"
+};
--- navigation/PlaceNavigation.js
+++ navigation/PlaceNavigation.js
@@ -5,13 +5,20 @@ import MapScreen from "../screens/MapScreen";
 import NewPlaceScreen from "../screens/NewPlaceScreen";
 import PlaceDetailScreen from "../screens/PlaceDetailScreen";
 import PlaceListScreen from "../screens/PlaceListScreen";
+import colors from "../constants/colors";
+
+const headerProp = {
+  headerStyle: {
+    backgroundColor: colors.primaryColor
+  }
+};
 
 const Stack = createStackNavigator();
 
 const PlaceNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={headerProp}>
         <Stack.Screen name="Places" component={PlaceListScreen} />
         <Stack.Screen name="Maps" component={MapScreen} />
         <Stack.Screen name="NewPlace" component={NewPlaceScreen} />
