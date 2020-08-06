a4ce4f9d37c2d4821f20f0049ccb8fa00316b02c
kinzlee <chinemeremnwoga@gmail.com>
2020-08-07 00:13:23 +0100

    rounding up navigation

--- screens/PlaceListScreen.js
+++ screens/PlaceListScreen.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
-const PlaceListScreen = () => {
+const PlaceListScreen = ({ navigation }) => {
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="add place"
+            iconName="md-add"
+            onPress={() => {
+              navigation.navigate("NewPlace");
+            }}
+          />
+        </HeaderButtons>
+      )
+    });
+  }, [navigation]);
+
   return (
     <View style={styles.screen}>
       <Text>This is the Place List screen</Text>
